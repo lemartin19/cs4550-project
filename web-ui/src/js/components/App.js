39cb67c43e1a665aa5b150f9fe56bec5fdf15f8e
lemartin19 <lmartin411@gmail.com>
2021-03-16 00:52:46 +0000

    begin setup

---
+++ web-ui/src/js/components/App.js
@@ -0,0 +1,17 @@
+'use es6';
+
+import React, { Component } from 'react';
+import Map from './Map';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Map />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ web-ui/src/js/components/Map.js
@@ -0,0 +1,8 @@
+'use es6';
+
+const Map = () => {
+  return null;
+};
+Map.displayName = 'Map';
+
+export default Map;
